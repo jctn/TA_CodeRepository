dfb2bf18120b805757df7da51255ff5ccfa2d66b
zxj <111>
2022-04-14 18:32:51 +0800

    更新：Dynamic Sky 添加：全局雾

---
+++ TA_CodeRepository/Assets/Scene/GlobalFog/GlobalFogFeature.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class GlobalFogFeature : ScriptableRendererFeature
+{
+    class GlobalFogPass : ScriptableRenderPass
+    {
+        const string CMDSTR = "GlobalFog";
+        RenderTargetHandle mTemporaryColorTexture;
+        RenderTargetIdentifier mSource;
+        Material mGlobalFogMat;
+
+        public GlobalFogPass()
+        {
+            mTemporaryColorTexture.Init("_TemporaryColorTexture");
+        }
+
+        public void Setup(RenderTargetIdentifier source, Material mat)
+        {
+            mSource = source;
+            mGlobalFogMat = mat;
+        }
+
+        protected RenderTextureDescriptor GetDescriptor(RenderTextureDescriptor descriptor, int width, int height)
+        {
+            RenderTextureDescriptor desc = descriptor;
+            desc.msaaSamples = 1;
+            desc.depthBufferBits = 0;
+            desc.width = width;
+            desc.height = height;
+            return desc;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (mGlobalFogMat == null) return;
+            CommandBuffer cmd = CommandBufferPool.Get(CMDSTR);
+            RenderTextureDescriptor camDesc = renderingData.cameraData.cameraTargetDescriptor;
+            RenderTextureDescriptor desc = GetDescriptor(camDesc, camDesc.width, camDesc.height);
+            cmd.GetTemporaryRT(mTemporaryColorTexture.id, desc, FilterMode.Bilinear);
+            cmd.Blit(mSource, mTemporaryColorTexture.Identifier(), mGlobalFogMat, 0);
+            cmd.Blit(mTemporaryColorTexture.Identifier(), mSource);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(mTemporaryColorTexture.id);
+        }
+    }
+
+    GlobalFogPass mGlobalFogPass;
+    Material mGlobalFogMat;
+
+    public override void Create()
+    {
+        mGlobalFogPass = new GlobalFogPass();
+        mGlobalFogPass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (renderingData.cameraData.camera.cameraType == CameraType.Game || renderingData.cameraData.camera.cameraType == CameraType.SceneView)
+        {
+            if(mGlobalFogMat == null)
+            {
+                Shader s = Shader.Find("Code Repository/Scene/GlobalFog");
+                if(mGlobalFogMat != null)
+                {
+                    mGlobalFogMat = new Material(s);
+                }           
+            }
+            mGlobalFogPass.Setup(renderer.cameraColorTarget, mGlobalFogMat);
+            renderer.EnqueuePass(mGlobalFogPass);
+        }
+    }
+}
